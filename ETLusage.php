cecb7021462d172a4ee8b0e24d1a5de8b192f0a2
Logamurugan <lpilavad@in.ibm.com>
2019-02-12 12:32:03 +0530

    Create ETLusage.php

---
+++ ETLusage.php
@@ -0,0 +1,10 @@
+<?php
+require_once('./ETL.php');
+
+$pdo = generate_pdo($fileName='./sqlite');
+
+// To dump the data 
+//import($pdo, './Sample.csv', ['delimiter'=>',', 'table'=>'sample']);
+
+//This will create a table: sample_no_null and a CSV sample_no_null.txt
+export($pdo, "select distinct id from sample WHERE id is not null", 'sample_no_null');
